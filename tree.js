6b543f98e601379e066004315eacbb783a289f49
AleixMJ <aleixtechIT@gmail.com>
2026-06-19 11:18:46 +0200

    completed the tree initialisation with buildTree() private method

--- tree.js
+++ tree.js
@@ -1,10 +1,41 @@
-
-
 class Node {
-
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
 }
 
 class Tree {
+    constructor(array) {
+        const cleanSorted = [...new Set(array)].toSorted((a, b) => a - b);
+        this.root = this.#buildTree(cleanSorted, 0, cleanSorted.length - 1);
+    }
+
+
+    #buildTree(array, start, end) {
+
+        if (start > end) {
+            return null;
+        }
+
+        const mid = Math.floor((start + end) / 2);
+        const node = new Node(array[mid]);
+
+        if (start === end) {
+            node.left = null;
+            node.right = null;
+            return node;
+            
+        }
+
+        node.left = this.#buildTree(array, start, mid - 1);
+        node.right = this.#buildTree(array, mid + 1, end);
+        
+        return node;
+    }
+        
+        
 
 }
 
